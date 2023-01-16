07b757a17f47700a06c1b64892b7c6961b13380e
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-16 19:00:28 +0100

    update root page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { page } from '$app/stores'
+	import PageTransition from '$lib/components/PageTransition.svelte'
+	import type { PageData } from './$types'
+
+	/**
+	 * Props.
+	 */
+	export let data: PageData
+</script>
+
+<PageTransition>
+	{#if !$page.data.session}
+		<h1>I am not logged in</h1>
+	{:else}
+		<h1>Welcome {$page.data.session.user.email}</h1>
+		<p>I am logged in!</p>
+		<code>{JSON.stringify(data.session?.user)}</code>
+	{/if}
+</PageTransition>
