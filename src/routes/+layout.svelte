06c8754ee0e9ddad3c6107287b2938f798b821f3
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-16 19:00:01 +0100

    update layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,68 @@
 <script lang="ts">
 	import '../app.css'
+	import { supabase } from '$lib/db'
+	import { goto, invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import type { PageData } from './$types'
+	import Navbar from './Navbar.svelte'
+
+	/**
+	 * Props.
+	 */
+	export let data: PageData
+
+	/**
+	 * State.
+	 */
+	const routes = [
+		{
+			name: 'Home',
+			href: '/'
+		},
+		{
+			name: 'Register',
+			href: '/register'
+		},
+		{
+			name: 'Login',
+			href: '/login'
+		},
+		{
+			name: 'Testpage',
+			href: '/testpage'
+		}
+	]
+
+	/**
+	 * Lifecycle methods.
+	 */
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth')
+		})
+
+		return () => {
+			subscription.unsubscribe()
+		}
+	})
+
+	/**
+	 * Methods.
+	 */
+	async function onLogout() {
+		const { error } = await supabase.auth.signOut()
+
+		if (error instanceof Error) {
+			throw error
+		}
+
+		goto('/login')
+	}
 </script>
 
+<Navbar
+	{routes}
+	on:logout={onLogout} />
 <slot />
