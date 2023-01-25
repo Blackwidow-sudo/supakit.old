78ddaabc852e30acbda641a3dc86da9713c4868d
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-25 22:35:40 +0100

    install static adapter and enable spa mode

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,9 @@ import { openRoutes } from '$lib/globals'
 import { redirect } from '@sveltejs/kit'
 import type { LayoutLoad } from './$types'
 
+// Enable prerender for SPA mode
+export const prerender = true
+
 export const load: LayoutLoad = async event => {
 	const { session } = await getSupabase(event)
 
