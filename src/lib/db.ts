2cb5ae3f8fcb7f2da700e4f0cbbe62e3c2f5f737
Merlin Cornehl <mcornehl@mkw.eu>
2023-01-30 19:29:31 +0100

    use static env-variables & enable SPA mode

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from '$env/dynamic/public'
+// import { env } from '$env/dynamic/public'
 // or use the static env
-// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
